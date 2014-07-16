45721bdbbd8fb264e05a02c1d6ec51f53b2eb11e
Nii <niiashikwei@hotmail.com>
2014-07-16 10:25:57 -0500

    Nii - some refactor to move away from public pages and use pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Beneshare::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'requests#index'
+  root 'pages#welcome'
 
   get 'requests' => 'requests#index'
 
